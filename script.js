e4c0bda2fe89b3edf9d713d72ac5bb12ea82f91d
Aashi19234 <aasthaj375@gmail.com>
2024-01-24 21:04:33 +0530

    api calling

--- script.js
+++ script.js
@@ -1,87 +1,27 @@
-const APIController = (function() {// IIFE function so that it is envoked immediately
+/*let username="PranavBarthwal"
+let URL=`https://api.github.com/users/${username}`
 
-    
-    const clientId = 'ADD YOUR CLIENT ID';
-    const clientSecret = 'ADD YOUR CLIENT SECRET';
+fetch(URL)
+  .then((response) => {
+      return response.json()
+  }).then((data) => {
+      console.log(data)
+      
+      //displayData(data.url, data.explanation, data.title, data.date, data.copyright )
+     
+  })
+  .catch((error) => console.log(error))*/
 
-    // private methods
-    const _getToken = async () => {// using fetch api to call the spotify token endpoints 
-// it is a post request , we will specify the content type and authorisation
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/x-www-form-urlencoded', 
-                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_credentials'
-        });
+  async function getAllusers(){
+    try{
 
-        const data = await result.json();
-        return data.access_token;
-    }
-
-const _getGenres = async (token) => {// this method receives a token parameter because we will need to supply the token spotify provided to us in ech API call, this function will return a romise denoted by the async keyword, we use the js fetch method to call the spotify category API endpoint per spotify as documentation this will be a get request and we will pass a bearer token along the way in the request header once we receive the result from spotify we will then convert it to json which will also return the promise in turn we await that conversion and store the data in variable called data finallu we return the itemas array store on category object.
-
-
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-
-    const data = await result.json();
-    return data.categories.items;
-}
-
-const _getPlaylistByGenre = async (token, genreId) => {
-//this method recieves a token and genre id parameter, we create a variable to hold the limit on the amount
-    const limit = 10;
-    
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-
-    const data = await result.json();
-    return data.playlists.items;
-}
-
-const _getTracks = async (token, tracksEndPoint) => {
-
-    const limit = 10;
-
-    const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-}
-
-const _getTrack = async (token, trackEndPoint) => {
-
-    const result = await fetch(`${trackEndPoint}`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-
-    const data = await result.json();
-    return data;
-}
-
-return {
-    getToken() {
-        return _getToken();
-    },
-    getGenres(token) {
-        return _getGenres(token);
-    },
-    getPlaylistByGenre(token, genreId) {
-        return _getPlaylistByGenre(token, genreId);
-    },
-    getTracks(token, tracksEndPoint) {
-        return _getTracks(token, tracksEndPoint);
-    },
-    getTrack(token, trackEndPoint) {
-        return _getTrack(token, trackEndPoint);
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=eISHzmGbQk7EMWv9NTlVtvzZVmeLKPUoL3uKMSPJ')
+    const data=response.json()
+    console.log(data);
+    } catch(error){
+        console.log("E: ", error);
+        
+        
     }
 }
-})();
-// i have to complete this, and get my api calls linked to the search bar.
+getAllusers()
